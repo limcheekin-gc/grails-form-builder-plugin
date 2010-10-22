56a81859a27c77b261eb4b78ed356c26093dbcd1
limcheekin@vobject.com <limcheekin@vobject.com@dec7328f-aa7e-381e-4102-47ba8060f7ca>
2010-10-22 02:46:26 +0000

    Initial import.

---
+++ trunk/FormBuilderGrailsPlugin.groovy
@@ -0,0 +1,75 @@
+/* Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ *
+ * @author <a href='mailto:limcheekin@vobject.com'>Lim Chee Kin</a>
+ *
+ * @since 0.1
+ */
+class FormBuilderGrailsPlugin {
+    // the plugin version
+    def version = "0.1"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.3.5 > *"
+    // the other plugins this plugin depends on
+    def dependsOn = [:]
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+            "grails-app/views/error.gsp"
+    ]
+
+    def author = "Lim Chee Kin"
+    def authorEmail = "limcheekin@vobject.com"
+    def title = "Grails Form Builder Plugin - Create Grails Form-based Application without Coding"
+    def description = '''\
+ The Grails Form Builder Plugin supports user to create online forms in web browser 
+ without any programming knowledge.
+ 
+ * Project Site and Documentation: http://code.google.com/p/grails-form-builder-plugin/
+ * Support: http://code.google.com/p/grails-form-builder-plugin/issues/list
+ * Discussion Forum: http://groups.google.com/group/grails-form-builder-plugin
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/form-builder"
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before 
+    }
+
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+}
