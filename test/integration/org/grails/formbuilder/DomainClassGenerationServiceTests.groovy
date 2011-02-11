c978f09bd94f12b5f1e2d683c5c3dcd2cc061e71
limcheekin@vobject.com <limcheekin@vobject.com@dec7328f-aa7e-381e-4102-47ba8060f7ca>
2011-02-11 09:47:45 +0000

    First import of two new domain classes: FreeMarkerTemplate and Domain. Also, initial import of domain class generation service.

---
+++ test/integration/org/grails/formbuilder/DomainClassGenerationServiceTests.groovy
@@ -0,0 +1,57 @@
+/* Copyright 2011 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.grails.formbuilder
+
+import grails.test.*
+
+/**
+*
+* @author <a href='mailto:limcheekin@vobject.com'>Lim Chee Kin</a>
+*
+* @since 0.1
+*/
+class DomainClassGenerationServiceTests extends GrailsUnitTestCase {
+	def domainClassGenerationService
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+  void testCreateSimpleDomainClass() {
+		Form form = new Form(name: 'bookForm')
+		form.fieldsList << new Field(
+			name: 'name',
+			type: 'SingleLineText',
+			settings: '{"required":true,"restriction":"alphanumeric"}'
+		)
+		assertEquals('name', 'bookForm.BookForm', domainClassGenerationService.getName(form))
+		String source = """\
+package bookForm
+class BookForm {
+String name
+Date dateCreated
+Date lastUpdated
+static constraints = {
+name nullable:false, blank:false, alphanumeric:true, maxSize:255
+dateCreated blank:false
+lastUpdated nullable:true
+}
+}"""
+		assertEquals('source', source, domainClassGenerationService.getSource(form))
+    }
+}
